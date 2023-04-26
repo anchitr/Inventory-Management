a4ce5a0e092d0a9f5d6224cf2653b2c5dfdcc269
Anchit Rao <anchitr@bu.edu>
2023-04-25 17:49:34 -0700

    wrote initial TODO list of development tasks

--- inventory-manager.py
+++ inventory-manager.py
@@ -7,3 +7,18 @@ Description of Project:
 This is a program that allows a manager of a store to view store inventory,
 product details, add new products, and monitor stock transactions.
 """
+# TODO:
+# - [x] Write program docstring
+# - Create Product class
+#   - Must have init() class method that takes at least 1 arg
+#   - Must have 1 private and 2 public class attributes
+#   - Must have 1 private and 2 public class methods
+#   - Must have __repr__ or __str__ class method
+#   - Must have one magic class method (not including above requirements)
+# - Identify best way of handling inventory control (dict v nested lists)
+# - Identify best method of storing product details (external csv v dict)
+# - Create starting list of products
+# - Design user prompts and input acceptance
+# - Design unittests for public class method testing
+#
+#
