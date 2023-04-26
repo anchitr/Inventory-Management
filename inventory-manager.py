5c0d66e0e88a6be43546dbb656d4a86969be4147
Anchit Rao <anchitr@bu.edu>
2023-04-25 17:27:22 -0700

    wrote docstring with program summary

---
+++ inventory-manager.py
@@ -0,0 +1,9 @@
+"""
+Anchit Rao
+Class: CS 521 - Spring 2
+Date: April 25, 2023
+Term Project
+Description of Project:
+This is a program that allows a manager of a store to view store inventory,
+product details, add new products, and monitor stock transactions.
+"""
