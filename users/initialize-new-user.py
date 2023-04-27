e6dae5eb78611d6580494cf4ced6d05293dcb2a3
Anchit Rao <anchit.rao.x@gmail.com>
2023-04-27 13:28:03 -0700

    created new file with init_new_user func

---
+++ users/initialize-new-user.py
@@ -0,0 +1,12 @@
+from user import User
+
+
+def init_new_user() -> list:
+    """This function will be executed upon running the inventory
+    management program and will request the user to enter their
+    information.
+
+    Returns:
+        list: The program will return a list of user info
+    """
+    pass
