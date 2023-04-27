4e76cbd1218ad0006f374fcfed2b4cd0ba1efe43
Anchit Rao <anchit.rao.x@gmail.com>
2023-04-27 15:55:11 -0700

    finished writing docstring for init_inventory func

---
+++ inventory/initialize_inventory.py
@@ -0,0 +1,12 @@
+def init_inventory(product_lst: list) -> list:
+    """This function takes a list of product objects
+    and creates a separate list of quantities and zips
+    the two lists together.
+
+    Args:
+        product_lst (list): List of product objects
+
+    Returns:
+        list: Zipped list of product objects and quantities
+    """
+    pass
