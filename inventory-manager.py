1da5d30c712219140d73d5271f7a988925b59713
Anchit Rao <anchit.rao.x@gmail.com>
2023-04-26 20:21:08 -0700

    updated todo comments with completed tasks

--- inventory-manager.py
+++ inventory-manager.py
@@ -7,13 +7,16 @@ Description of Project:
 This is a program that allows a manager of a store to view store inventory,
 product details, add new products, and monitor stock transactions.
 """
+
+from product import Product
+
 # TODO:
 # - [x] Write program docstring
 # - Create Product class
-#   - Must have init() class method that takes at least 1 arg
-#   - Must have 1 private and 2 public class attributes
-#   - Must have 1 private and 2 public class methods
-#   - Must have __repr__ or __str__ class method
+#   - [x] Must have init() class method that takes at least 1 arg
+#   - [x] Must have 1 private and 2 public class attributes
+#   - [x] Must have 1 private and 2 public class methods
+#   - [x] Must have __repr__ or __str__ class method
 #   - Must have one magic class method (not including above requirements)
 # - Identify best way of handling inventory control (dict v nested lists)
 # - Identify best method of storing product details (external csv v dict)
