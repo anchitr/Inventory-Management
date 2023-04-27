d17376423a9f0ca6df433f79219b352253377ba9
Anchit Rao <anchit.rao.x@gmail.com>
2023-04-27 14:12:47 -0700

    created view_all_products func with docstring

---
+++ viewing/view-all-products.py
@@ -0,0 +1,12 @@
+def view_all_products(product_lst: list) -> list:
+    """This function takes in a list of product objects
+    and prints the __str__ method for all of them while
+    storing the strings into a list
+
+    Args:
+        product_lst (list): List of product Objects
+
+    Returns:
+        list: List of product info strings
+    """
+    pass
