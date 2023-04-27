03ba7897276ed10d626995a987d41e4b5ec1aaa5
Anchit Rao <anchit.rao.x@gmail.com>
2023-04-27 15:47:58 -0700

    updated dev task checklist

--- inventory_management_program.py
+++ inventory_management_program.py
@@ -9,15 +9,18 @@ product details, add new products, and monitor stock transactions.
 """
 # TODO:
 # - [x] Write program docstring
-# - Create Product class
-#   - Must have init() class method that takes at least 1 arg
-#   - Must have 1 private and 2 public class attributes
-#   - Must have 1 private and 2 public class methods
-#   - Must have __repr__ or __str__ class method
-#   - Must have one magic class method (not including above requirements)
+# - [x] Create Product class
+#   - [x] Must have init() class method that takes at least 1 arg
+#   - [x] Must have 1 private and 2 public class attributes
+#   - [x] Must have 1 private and 2 public class methods
+#   - [x] Must have __repr__ or __str__ class method
+#   - [x] Must have one magic class method (not including above requirements)
+# - Create User class
+#   - [x] Private password attribute
+#   - [x] Public name update method
 # - Identify best way of handling inventory control (dict v nested lists)
-# - Identify best method of storing product details (external csv v dict)
-# - Create starting list of products
+# - [x] Identify best method of storing product details (external csv v dict)
+# - [x] Create starting list of products
 # - Design user prompts and input acceptance
 # - Design unittests for public class method testing
 #
