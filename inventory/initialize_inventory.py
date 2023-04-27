0bb90702025f79d6199493afad3c9c811bba4856
Anchit Rao <anchit.rao.x@gmail.com>
2023-04-27 15:47:19 -0700

    created init_inventory func with docstring

---
+++ inventory/initialize_inventory.py
@@ -0,0 +1,10 @@
+def init_inventory(product_lst: list) -> list:
+    """This function takes a list of products and
+
+    Args:
+        product_lst (list): _description_
+
+    Returns:
+        list: _description_
+    """
+    pass
