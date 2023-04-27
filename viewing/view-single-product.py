8f141dddc215fb0d304e5275efc1acb041c5ace0
Anchit Rao <anchit.rao.x@gmail.com>
2023-04-27 14:35:00 -0700

    created view_single_product func with docstring

---
+++ viewing/view-single-product.py
@@ -0,0 +1,14 @@
+def view_single_product(product_lst: list) -> str:
+    """This function takes in a list of product objects and
+    prompts the user to enter the product id of the product whose
+    info he is searching for. The function then locates the product
+    with the id entered by the user and returns the __str__ method
+    for that specific product object.
+
+    Args:
+        product_lst (list): List of product objects
+
+    Returns:
+        str: Output of product info
+    """
+    pass
