9cfccfa327800b0f62e8e82a841ebe41f7ad8ea2
Anchit Rao <anchit.rao.x@gmail.com>
2023-04-27 14:24:27 -0700

    added comments for readability

--- products/price-comparison.py
+++ products/price-comparison.py
@@ -2,6 +2,7 @@ def compare_price(products: list) -> str:
     for i in products:
         print(f"{i.product_id}: {i.name}")
 
+    # Retrieve user input for the two products being compared
     try:
         user_product1_id = int(
             input("Please enter the id of the first comparison product: ")
@@ -10,13 +11,17 @@ def compare_price(products: list) -> str:
             input("Please enter the id of the second comparison product: ")
         )
 
+    # Handle errors if user enters id that doesn't exist in the product list
     except IndexError:
         return "One of the Product ID's is invalid. Please try again."
 
+    # Store first product object
     first_product = products[user_product1_id - 1]
+
+    # Store second product object
     second_product = products[user_product2_id - 1]
 
-    first_product > second_product
+    first_product > second_product  # Compare prices of products
 
     if True:
         return (
