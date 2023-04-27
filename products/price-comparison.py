b6bf1e293bd1340cf6e993f40446f52f387a7d69
Anchit Rao <anchit.rao.x@gmail.com>
2023-04-27 10:42:14 -0700

    created function for price comparison in new file

---
+++ products/price-comparison.py
@@ -0,0 +1,26 @@
+from products import starting-products
+
+def compare_price(products: list) -> str:
+        for i in products:
+            print(f"{i.product_id}: {i.name}")
+
+        try:
+            user_product1_id = int(
+                input("Please enter the id of the first comparison product: ")
+            )
+            user_product2_id = int(
+                input("Please enter the id of the second comparison product: ")
+            )
+
+        except IndexError:
+            return "One of the Product ID's is invalid. Please try again."
+
+        product_lst[user_product1_id - 1] > product_lst[user_product2_id - 1]
+
+        if True:
+            return (
+                f"Product {user_product1_id} is more expensive than {user_product2_id}"
+            )
+
+        else:
+            return f"Product {user_product1_id} is cheaper than {user_product2_id}"
