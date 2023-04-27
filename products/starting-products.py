a02cce8d2c25300e69bf831a999aff34e48c89bd
Anchit Rao <anchit.rao.x@gmail.com>
2023-04-27 10:34:42 -0700

    created file for function initializing product list

---
+++ products/starting-products.py
@@ -0,0 +1,8 @@
+def init_product_list() -> list:
+    """This function initializes 5 products to start as part
+    of the store's inventory
+
+    Returns:
+        list: A list of product objects
+    """
+    pass
