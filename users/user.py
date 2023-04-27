ede123f79e86b6ad16be075f3d76fc862392da63
Anchit Rao <anchit.rao.x@gmail.com>
2023-04-27 13:00:20 -0700

    created new User class with docstring

---
+++ users/user.py
@@ -0,0 +1,15 @@
+class User:
+    """This class is meant to store information
+    about the user currently operating the program
+    """
+
+    def __init__(self, first_name: str, last_name: str, password: str) -> None:
+        """This constructor takes the user's info as arguments and stores
+        it for viewing and confirming actions in the main program
+
+        Args:
+            first_name (str): First name of the user
+            last_name (str): Last name of the user
+            password (str): Password decided by user (should be simple)
+        """
+        pass
