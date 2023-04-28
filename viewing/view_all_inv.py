eae0a66a78e5559aef0169a425148e11b3cc6f40
Anchit Rao <anchit.rao.x@gmail.com>
2023-04-27 17:21:14 -0700

    finished view_all_inventory func

--- viewing/view_all_inv.py
+++ viewing/view_all_inv.py
@@ -9,4 +9,18 @@ def view_all_inventory(inventory_lst: list) -> list:
         list: List of strings containing product id, name,
         and quantity in inventory
     """
-    pass
+    # Initialize empty list to store formatted strings
+    all_inventory_info = []
+
+    # Create header row
+    headers = f"{'ID' : <5}{'Name' : <15}{'Quantity' : <5}"
+
+    all_inventory_info.append(headers)  # Insert header row string
+
+    for lst in inventory_lst:
+        # Insert formatted string of id, name, and quantity
+        output = f"{lst[0].product_id : <5}{lst[0].name : ^15}{lst[1] : ^5}"
+
+        all_inventory_info.append(output)  # Append to final list
+
+    return all_inventory_info
