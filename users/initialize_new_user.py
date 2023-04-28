ee3336602383a45a3c2ceebe42616a1c8d7dd1e6
Anchit Rao <anchit.rao.x@gmail.com>
2023-04-27 21:23:47 -0700

    reformatted user prompts for init_new_user

--- users/initialize_new_user.py
+++ users/initialize_new_user.py
@@ -9,17 +9,17 @@ def init_new_user() -> object:
     Returns:
         list: The program will return a list of user info
     """
-    user_name = input("Please enter your first and last name separated by a space: ")
-    names = user_name.split(" ")
 
-    if len(names) == 2:
-        first_name = names[0]
-        last_name = names[1]
-    else:
-        return "Invalid name input. Please try again."
+    # Get user first and last names
+    user_first_name = input("Please enter your first name: ")
 
+    user_last_name = input("Please enter your last name: ")
+
+    # Get user password
     password = input("Please enter a simple password: ")
 
-    new_user = User(first_name=first_name, last_name=last_name, password=password)
+    new_user = User(
+        first_name=user_first_name, last_name=user_last_name, password=password
+    )
 
     return new_user
