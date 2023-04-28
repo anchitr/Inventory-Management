ad5e90cb15dcf6de9d3ca0120f445a01819ce861
Anchit Rao <anchit.rao.x@gmail.com>
2023-04-28 09:11:11 -0700

    added comments for clarity in change_name func

--- updating/update_prod_name.py
+++ updating/update_prod_name.py
@@ -9,18 +9,24 @@ def change_name(product_lst: list) -> str:
     Returns:
         str: Output message of change in name
     """
+    # Catch invalid input
     try:
         # Prompt user for which product they want to update
         user_pid_input = int(input("\nPlease enter the Product ID of the product: "))
 
+    except ValueError:
+        return "\nInvalid input. Please try again."
+        # Assign the product object from the list to a variable
+
+    # Catch invalid product id
+    try:
         # Assign the product object from the list to a variable
         product = product_lst[user_pid_input - 1]
 
-    # Return error string in case user enters invalid product id
     except IndexError:
         return "\nProduct ID does not exist. Please try again."
 
-    # Prompt the user to enter the new price and convert value to float
+    # Prompt the user to enter the new name
     new_name = input("\nPlease enter the new name: ")
 
     # Call update_price class method of Product
