be80b3eadf060c0025f5b83331675c5450c2a2a7
Anchit Rao <anchit.rao.x@gmail.com>
2023-04-27 21:04:05 -0700

    created account_setup func with docstring

---
+++ handler_funcs/new_account.py
@@ -0,0 +1,12 @@
+from ..users import initialize_new_user
+
+
+def account_setup() -> object:
+    """This function will keep attempting to run the
+    init_new_user func from initialize_new_user.py until
+    an object is returned with no errors
+
+    Returns:
+        object: _description_
+    """
+    pass
