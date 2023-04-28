9da917182421a618fac0abdfab7f2417483e04e6
Anchit Rao <anchit.rao.x@gmail.com>
2023-04-28 08:34:29 -0700

    created get_supp_info func with docstring

---
+++ viewing/view_supplier_info.py
@@ -0,0 +1,14 @@
+def get_supp_info(product_lst: list) -> str:
+    """This function takes in a list of products, prompts the
+    user for input on which product id they want to search.
+    Once they enter a valid product id, the system calls on
+    the private class method __view_supplier_details() and returns
+    a string of the info
+
+    Args:
+        product_lst (list): List of product objects
+
+    Returns:
+        str: String of supplier info for product
+    """
+    pass
