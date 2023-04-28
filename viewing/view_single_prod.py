acb7b0fd7a394cd781d3f6b4d0b68dcb11e03915
Anchit Rao <anchit.rao.x@gmail.com>
2023-04-28 09:25:25 -0700

    added comments for readability in view_single_product

--- viewing/view_single_prod.py
+++ viewing/view_single_prod.py
@@ -9,18 +9,22 @@ def view_single_product(product_lst: list) -> str:
         product_lst (list): List of product objects
 
     Returns:
-        str: Output of product info
+        str: Output of product info using __str__ method
     """
 
     try:
+        # Prompt user for the id of the product they want to view
         user_pid_input = int(input("Please enter the Product ID of the product: "))
 
+    # Catch invalid user input for product id
     except ValueError:
         return "\nInvalid input. Please try again."
 
     try:
+        # Assign the product object from the list to a variable
         user_product = product_lst[user_pid_input - 1]
 
+    # Return error string in case user enters invalid product id
     except IndexError:
         return "\nProduct ID does not exist. Please try again."
 
