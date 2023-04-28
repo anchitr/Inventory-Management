fa3850a747596a930511f3193467d48339b7eb6d
Anchit Rao <anchit.rao.x@gmail.com>
2023-04-27 17:05:48 -0700

    created view_all_inventory func with docstring

---
+++ viewing/view_all_inv.py
@@ -0,0 +1,11 @@
+def view_all_inventory(inventory_lst: list) -> str:
+    """This function loops through a list of lists and presents
+    the product id, name, and current quantity
+
+    Args:
+        inventory_lst (list): List of product objects and quantities
+
+    Returns:
+        str: Formatted string of product id, name, and quantity in inventory
+    """
+    pass
