fdd3c3cd819e71d319c7c2696bff57b52225e2e1
Anchit Rao <anchit.rao.x@gmail.com>
2023-04-28 13:08:01 -0700

    created inv_prod_update func with docstring

---
+++ updating/update_inv.py
@@ -0,0 +1,13 @@
+def inv_prod_update(inventory_lst: list, product_lst: list) -> list:
+    """This function will be called after adding a new product to create
+    a new entry in the inventory with the starting quantity of 10. It
+    returns a list of product object and quantity lists.
+
+    Args:
+        inventory_lst (list): List of lists (product obj, quantity)
+        product_lst (list): List of product objects
+
+    Returns:
+        list: _description_
+    """
+    pass
