ccb266f67e6c23042755901cbc4ddc853f6d010e
Anchit Rao <anchit.rao.x@gmail.com>
2023-04-28 12:42:54 -0700

    created new_product func with docstring

---
+++ updating/add_new_product.py
@@ -0,0 +1,13 @@
+def new_product(product_lst: list) -> list:
+    """This function takes a list of product objects,
+    initializes a new instance of the Product class and
+    appends the resulting object into the list and returns
+    it.
+
+    Args:
+        product_lst (list): List of product objects
+
+    Returns:
+        list: List of product objects with new object
+    """
+    pass
