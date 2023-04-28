0841d63d0f130dbaf7e609dc69fedd4674869fe6
Anchit Rao <anchit.rao.x@gmail.com>
2023-04-27 19:08:34 -0700

    split greeting and salution funcs into separate files

---
+++ greeters/salutation.py
@@ -0,0 +1,8 @@
+def good_bye() -> str:
+    """Generic salutation function to print out a goodbye
+    once user chooses to quit the program
+
+    Returns:
+        str: Goodbye message
+    """
+    return "\nThank you for using the Inventory Management Program! Have a nice day."
