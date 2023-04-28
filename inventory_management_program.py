59147f646c26e8d5682864ef7cb5539f99c351ce
Anchit Rao <anchitr@bu.edu>
2023-04-28 15:54:18 -0700

    removed TODO task list

--- inventory_management_program.py
+++ inventory_management_program.py
@@ -22,26 +22,6 @@ from updating import (
 )
 
 
-# TODO:
-# - [x] Write program docstring
-# - [x] Create Product class
-#   - [x] Must have init() class method that takes at least 1 arg
-#   - [x] Must have 1 private and 2 public class attributes
-#   - [x] Must have 1 private and 2 public class methods
-#   - [x] Must have __repr__ or __str__ class method
-#   - [x] Must have one magic class method (not including above requirements)
-# - Create User class
-#   - [x] Private password attribute
-#   - [x] Public name update method
-# - Identify best way of handling inventory control (dict v nested lists)
-# - [x] Identify best method of storing product details (external csv v dict)
-# - [x] Create starting list of products
-# - Design user prompts and input acceptance
-# - Design unittests for public class method testing
-#
-#
-
-
 def main_menu() -> str:
     """Function that presents the main menu choices
     for the user
